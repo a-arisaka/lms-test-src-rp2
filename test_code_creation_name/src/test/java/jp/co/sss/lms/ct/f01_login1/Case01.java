38220fb5e166c9562c85bc18e17dfefc5cb6989f
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-22 13:42:09 +0900

    fix テストメソッドの作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,7 +1,11 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +13,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +24,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース01 ログイン画面への遷移")
 public class Case01 {
+	//	ChromeDriver chromeDriver = new ChromeDriver();
 
 	/** 前処理 */
 	@BeforeAll
@@ -34,8 +41,12 @@ public class Case01 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-
+	void test01() throws Exception {
+		webDriver.get("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		Thread.sleep(5000);
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("C:\\work\\ScreenShot_practice\\login_success.png"));
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,7 +1,11 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +13,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +27,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース02 受講生 ログイン 認証失敗")
 public class Case02 {
+	ChromeDriver chromeDriver = new ChromeDriver();
 
 	/** 前処理 */
 	@BeforeAll
@@ -34,15 +44,42 @@ public class Case02 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		webDriver.get("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//ログインIDとパスワード入力欄が表示されているかチェック
+		WebElement elemUser = webDriver.findElement(By.id("loginId"));
+		WebElement elemPass = webDriver.findElement(By.id("password"));
+		assertTrue(elemUser.isDisplayed());
+		assertTrue(elemPass.isDisplayed());
+
+		Thread.sleep(5000);
+
+		//スクリーンショットをフォルダに格納
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("C:\\work\\ScreenShot_practice\\case2_login.png"));
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		//DBに登録されていないIDとパスワードでログイン
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAB");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAB");
+		//ログインを実行
+		webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/form/fieldset/div[3]/div/input")).click();
+
+		//エラーメッセージ「ログインに失敗しました。」が表示されることをチェック
+		WebElement errorMsg = webDriver
+				.findElement(By.xpath("//*[@id=\"main\"]/div[1]/form/span"));
+
+		assertTrue(errorMsg.isDisplayed());
+
+		//スクリーンショットをフォルダに格納
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("C:\\work\\ScreenShot_practice\\login_faild.png"));
+
 	}
 
 }
