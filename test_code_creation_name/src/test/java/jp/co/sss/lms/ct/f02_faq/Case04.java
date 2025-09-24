c4856aa98721471e154033aa958b78b32177a392
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-24 11:02:21 +0900

    fix case4のテストメソッド作成
    実行に伴いevidenceフォルダに追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -4,6 +4,7 @@ import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
@@ -14,9 +15,11 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 /**
  * 結合テスト よくある質問機能
@@ -87,15 +90,50 @@ public class Case04 {
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
-	void test03() {
-		// TODO ここに追加
+	void test03() throws Exception {
+		//機能一覧をクリック
+		webDriver.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[1]/li[4]/a")).click();
+		Thread.sleep(300);
+		//ヘルプを選択、クリック
+		webDriver.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[1]/li[4]/ul/li[4]/a")).click();
+		//遷移したページがヘルプページであるか確認
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+		//スクリーンショットをevidenceフォルダに保存
+
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		String destinationPath = "evidence/case04_03" + ".png";
+		File destFile = new File(destinationPath);
+
+		FileUtils.copyFile(file, destFile);
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
-	void test04() {
-		// TODO ここに追加
+	void test04() throws Exception {
+		// 元のタブのハンドル
+		String originalTab = webDriver.getWindowHandle();
+		// 新しいタブで開きたいリンク要素を取得
+		WebElement link = webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div[2]/p/a"));
+		// ActionsクラスでCtrl+Clickを実行
+		Actions actions = new Actions(webDriver);
+		actions.keyDown(Keys.CONTROL).click(link).keyUp(Keys.CONTROL).build().perform();
+
+		Thread.sleep(2000);
+
+		//タブの切り替え
+		ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+		tabs.remove(originalTab);
+		webDriver.switchTo().window(tabs.get(0));
+		//スクリーンショットをevidenceフォルダに保存
+
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		String destinationPath = "evidence/case04_04" + ".png";
+		File destFile = new File(destinationPath);
+
+		FileUtils.copyFile(file, destFile);
+
 	}
 
 }
