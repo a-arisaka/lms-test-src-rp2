de217513b2b13eb850cb3c4f42a02f14e9b329fe
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-25 14:43:30 +0900

    fix case7の編集
    テスト実行に伴いevidenceにキャプチャを追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,7 +1,13 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +15,13 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト レポート機能
@@ -34,36 +47,114 @@ public class Case07 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		webDriver.get("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//ログインIDとパスワード入力欄が表示されているかチェック
+		WebElement elemUser = webDriver.findElement(By.id("loginId"));
+		WebElement elemPass = webDriver.findElement(By.id("password"));
+		assertTrue(elemUser.isDisplayed());
+		assertTrue(elemPass.isDisplayed());
+
+		Thread.sleep(5000);
+
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		String destinationPath = "evidence/case07_01" + ".png";
+		File destFile = new File(destinationPath);
+		FileUtils.copyFile(file, destFile);
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		//初回ログイン済みのパスワードでログイン
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.clear();
+		password.sendKeys("StudentAA01a");
+		//ログインを実行
+		webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/form/fieldset/div[3]/div/input")).click();
+
+		//メッセージ「ようこそ受講生AA1さん」が表示されることを確認
+		WebElement welcomeUser = webDriver
+				.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[2]/li[2]/a"));
+
+		assertTrue(welcomeUser.isDisplayed());
+
+		//スクリーンショットをevidenceフォルダに保存
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		String destinationPath = "evidence/case07_02" + ".png";
+		File destFile = new File(destinationPath);
+
+		FileUtils.copyFile(file, destFile);
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
-	void test03() {
-		// TODO ここに追加
+	void test03() throws Exception {
+		WebElement button = webDriver.findElement(By.xpath("(//input[@value='詳細'])[3]"));
+
+		JavascriptExecutor js = (JavascriptExecutor) webDriver;
+		js.executeScript("arguments[0].click();", button);
+
+		Thread.sleep(300);
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+		//スクリーンショットをevidenceフォルダに保存
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		String destinationPath = "evidence/case07_03" + ".png";
+		File destFile = new File(destinationPath);
+
+		FileUtils.copyFile(file, destFile);
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
-	void test04() {
-		// TODO ここに追加
+	void test04() throws Exception {
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		By submissionButtonLocator = By.xpath("//input[@value='日報【デモ】を提出する']");
+		WebElement submissionButton = wait.until(ExpectedConditions.elementToBeClickable(submissionButtonLocator));
+		submissionButton.click();
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+		//スクリーンショットをevidenceフォルダに保存
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		String destinationPath = "evidence/case07_04" + ".png";
+		File destFile = new File(destinationPath);
+
+		FileUtils.copyFile(file, destFile);
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
-	void test05() {
-		// TODO ここに追加
+	void test05() throws Exception {
+		//テキストエリアに詳細を入力
+		WebElement detailTextArea = webDriver.findElement(By.xpath("//*[@id=\"content_0\"]"));
+		detailTextArea.sendKeys("本日の練習問題を完了");
+		//提出ボタンをクリック
+		WebElement submissionButton = webDriver.findElement(By.xpath("//button[text()='提出する']"));
+		submissionButton.click();
+
+		Thread.sleep(300);
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		//ボタンが更新されているかチェック
+		WebElement button = webDriver.findElement(By.xpath("(//input[@value='提出済み日報【デモ】を確認する'])"));
+		assertTrue(button.isDisplayed());
+
+		//スクリーンショットをevidenceフォルダに保存
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		String destinationPath = "evidence/case07_05" + ".png";
+		File destFile = new File(destinationPath);
+
+		FileUtils.copyFile(file, destFile);
+
 	}
 
 }
