4758632cb2b567bca5a6cb6063456f35a438185b
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-26 16:36:08 +0900

    fix case10作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,8 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +11,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -34,36 +41,95 @@ public class Case10 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		webDriver.get("http://localhost:8080/lms/");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//ログインIDとパスワード入力欄が表示されているかチェック
+		WebElement elemUser = webDriver.findElement(By.id("loginId"));
+		WebElement elemPass = webDriver.findElement(By.id("password"));
+		assertTrue(elemUser.isDisplayed());
+		assertTrue(elemPass.isDisplayed());
+		Thread.sleep(5000);
+
+		// ページのキャプチャを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		//初回ログイン済みのパスワードでログイン
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.clear();
+		password.sendKeys("StudentAA01a");
+		//ログインを実行
+		webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/form/fieldset/div[3]/div/input")).click();
+
+		//メッセージ「ようこそ受講生AA1さん」が表示されることを確認
+		WebElement welcomeUser = webDriver
+				.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[2]/li[2]/a"));
+
+		assertTrue(welcomeUser.isDisplayed());
+
+		// ページのキャプチャを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
-	void test03() {
-		// TODO ここに追加
+	void test03() throws Exception {
+		WebElement attendanceButton = webDriver.findElement(By.xpath("//a[@href='/lms/attendance/detail']"));
+		attendanceButton.click();
+		Thread.sleep(300);
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+		// ページのキャプチャを取得する
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+		WebElement checkin = webDriver.findElement(By.xpath("//input[@value='出勤']"));
+		assertTrue(checkin.isDisplayed());
+		checkin.click();
+		Alert alert = WebDriverUtils.webDriver.switchTo().alert();
+		alert.accept();
+		pageLoadTimeout(10);
+		//遷移先のURLを確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/attendance/detail"));
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
-	}
+		WebElement checkout = webDriver.findElement(By.xpath("//input[@value='退勤']"));
+		assertTrue(checkout.isDisplayed());
+		checkout.click();
+		Alert alert = WebDriverUtils.webDriver.switchTo().alert();
+		//ダイアログ「OK」ボタン押下
+		alert.accept();
+		pageLoadTimeout(10);
+		//属性値が変化しているか確認
+		String classText = WebDriverUtils.webDriver.findElement(By.xpath("//div[@class='main2clm']/div"))
+				.getAttribute("class");
+		assertEquals("alert alert-info alert-dismissible fade in", classText);
+		getEvidence(new Object() {
+		});
 
+	}
 }
