7287cc754715d5526473cb762638143c0c6faaa3
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-29 15:35:39 +0900

    fix case1 メソッドの簡略化

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -3,9 +3,6 @@ package jp.co.sss.lms.ct.f01_login1;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.Assert.*;
 
-import java.io.File;
-
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +10,6 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 /**
  * 結合テスト ログイン機能①
@@ -42,17 +37,11 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() throws Exception {
-		webDriver.get("http://localhost:8080/lms/");
-		assertEquals("ログイン | LMS", webDriver.getTitle());
-		Thread.sleep(5000);
-
-		//スクリーンショットをevidenceフォルダに保存
-		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		String destinationPath = "evidence/case01" + ".png";
-		File destFile = new File(destinationPath);
-
-		FileUtils.copyFile(file, destFile);
-
+		goTo("http://localhost:8080/lms/");
+		String pageTitle = webDriver.getTitle();
+		assertEquals("ログイン | LMS", pageTitle);
+		getEvidence(new Object() {
+		});
 	}
 
 }
